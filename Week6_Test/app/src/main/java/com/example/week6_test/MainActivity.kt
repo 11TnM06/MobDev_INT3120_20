c029f0182e687e087831aef9a9a494f870c8c19d
Pswrd <hoangducmanh123@gmail.com>
2023-10-04 01:18:03 +0700

    update week6

---
+++ Week6_Test/app/src/main/java/com/example/week6_test/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.week6_test
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    var CONTENT_URI = Uri.parse("content://com.demo.user.provider/users")
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickShowDetails(view: View?) {
+        // inserting complete table details in this text field
+        val resultView = findViewById<View>(R.id.res) as TextView
+
+        // creating a cursor object of the
+        // content URI
+        val cursor = contentResolver.query(Uri.parse("content://com.demo.user.provider/users"), null, null, null, null)
+
+        // iteration of the cursor
+        // to print whole table
+        if (cursor!!.moveToFirst()) {
+            val strBuild = StringBuilder()
+            while (!cursor.isAfterLast) {
+                val indexId = cursor.getColumnIndex("id")
+                val indexName = cursor.getColumnIndex("name")
+                strBuild.append("""
+	
+	${cursor.getString(indexId)}-${cursor.getString(indexName)}
+	""".trimIndent())
+                cursor.moveToNext()
+            }
+            resultView.text = strBuild
+        } else {
+            resultView.text = "No Records Found"
+        }
+    }
+}
